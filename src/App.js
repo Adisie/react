bf9348e924596f71e514aa7c089ae524776a9bfc
Adisie Fenta <adisiefenta23@gmail.com>
2023-10-15 15:27:35 +0300

    sass training

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import Header from './components/Header';
 
 
 class App extends React.Component {
     render(){
         return (
             <div className='container'>
-                <h3>Ethiopia The Land Of Wisdom</h3>
+                <Header username = "Haddis_Fun" />
             </div>
         )
     }
