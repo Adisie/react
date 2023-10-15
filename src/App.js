5733b242e9cacad58e1fd333cb8f57716293d985
Adisie Fenta <adisiefenta23@gmail.com>
2023-10-15 16:17:17 +0300

    start reacting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import profile_image from "./assets/tewodiros1.jpg"
-
-import "./css/index.css";
 
 class App extends React.Component {
     render(){
         return (
             <div className='container'>
-                <header>
-                    <a href='' className='site-logo'>highland</a>
-                    <nav className='navigation'>
-                        <ul>
-                            <li><a href=''>Home</a></li>
-                            <li><a href=''>Experience</a></li>
-                            <li><a href=''>Skills</a></li>
-                            <li><a href=''>Contact</a></li>
-                        </ul>
-                        <img src={profile_image} className='user-profile-img' />
-                    </nav>
-                </header>
+                <h3>react</h3>
             </div>
         )
     }
