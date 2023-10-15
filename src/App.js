edf511883fef89141c8e3486bcd538e4098e1391
Adisie Fenta <adisiefenta23@gmail.com>
2023-10-15 15:45:53 +0300

    sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import Header from './components/Header';
+import profile_image from "./assets/tewodiros1.jpg"
 
+import "./css/index.css";
 
 class App extends React.Component {
     render(){
         return (
             <div className='container'>
-                <Header username = "Haddis_Fun" />
+                <header>
+                    <a href='' className='site-logo'>highland</a>
+                    <nav className='navigation'>
+                        <ul>
+                            <li><a href=''>Home</a></li>
+                            <li><a href=''>Experience</a></li>
+                            <li><a href=''>Skills</a></li>
+                            <li><a href=''>Contact</a></li>
+                        </ul>
+                        <img src={profile_image} className='user-profile-img' />
+                    </nav>
+                </header>
             </div>
         )
     }
